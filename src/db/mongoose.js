1d7c21b14b325664cfa9251c31558e97be9c1402
Nadirsha Aliyar <42641677+nadirshaaliyar@users.noreply.github.com>
2022-10-30 15:56:48 +0530

    Creating mongoose model🪪

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,23 +2,43 @@ const mongoose =require('mongoose')
 
 mongoose.connect('mongodb+srv://task-app:task-app@cluster0.e7dk1su.mongodb.net/?retryWrites=true&w=majority')
 
-const User = mongoose.model('User',{
-    name: {
+// const User = mongoose.model('User',{
+//     name: {
+//         type:String
+
+//     },
+//     age:{
+//         type:Number
+//     }
+// })
+ const Task =mongoose.model('Task',{
+    description:{
         type:String
-
     },
-    age:{
-        type:Number
+    completed:{
+        type:Boolean
     }
+} )
+
+const task =new Task({
+    description:'Learn mongoose library',
+    completed:false
 })
 
-const me =new User({
-    name: 'Nadir',
-    age:22
-})
-
-me.save().then(() =>{
-    console.log(me)
+task.save().then(()=>{
+    console.log(task)
 }).catch((error)=>{
-    console.log('Error!',error)
+    console.log(error)
 })
+
+
+// const me =new User({
+//     name: 'Nadir',
+//     age:22
+// })
+
+// me.save().then(() =>{
+//     console.log(me)
+// }).catch((error)=>{
+//     console.log('Error!',error)
+// })
