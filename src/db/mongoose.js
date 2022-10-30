93a683661877623e8fffb3d77fcdcef435ba4c09
Nadirsha Aliyar <42641677+nadirshaaliyar@users.noreply.github.com>
2022-10-30 16:28:05 +0530

    validation and sanitiztion-I

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,44 +1,69 @@
 const mongoose =require('mongoose')
-
+const validator =require('validator')
 mongoose.connect('mongodb+srv://task-app:task-app@cluster0.e7dk1su.mongodb.net/?retryWrites=true&w=majority')
 
-// const User = mongoose.model('User',{
-//     name: {
-//         type:String
+const User = mongoose.model('User',{
+    name: {
+        type:String,
+        required:true,
+        trim:true
 
-//     },
-//     age:{
-//         type:Number
-//     }
-// })
- const Task =mongoose.model('Task',{
-    description:{
-        type:String
     },
-    completed:{
-        type:Boolean
+    email: {
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        validate(value){
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is Invalid!')
+            }
+        }
+    },
+    age:{
+        type:Number,
+        default:0,
+        validate(value){
+            if(value<0){
+                throw new Error('Age must be a postive number')
+            }
+        }
     }
-} )
-
-const task =new Task({
-    description:'Learn mongoose library',
-    completed:false
 })
 
-task.save().then(()=>{
-    console.log(task)
+const me =new User({
+    name: '   Nadir  ', 
+    email:'    Nadir@gmail.com  '
+    
+})
+
+me.save().then(() =>{
+    console.log(me)
 }).catch((error)=>{
-    console.log(error)
+    console.log('Error!',error)
 })
 
 
-// const me =new User({
-//     name: 'Nadir',
-//     age:22
+
+
+//  const Task =mongoose.model('Task',{
+//     description:{
+//         type:String
+//     },
+//     completed:{
+//         type:Boolean
+//     }
+// } )
+
+// const task =new Task({
+//     description:'Learn mongoose library',
+//     completed:false
 // })
 
-// me.save().then(() =>{
-//     console.log(me)
+// task.save().then(()=>{
+//     console.log(task)
 // }).catch((error)=>{
-//     console.log('Error!',error)
+//     console.log(error)
 // })
+
+
