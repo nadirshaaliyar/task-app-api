fc13193e9595771b01be254294afe20b6b7cacff
Nadirsha Aliyar <42641677+nadirshaaliyar@users.noreply.github.com>
2022-11-01 16:28:37 +0530

    rest api for user creation🚀

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,63 +1,7 @@
 const mongoose =require('mongoose')
-const validator =require('validator')
+
+
 mongoose.connect('mongodb+srv://task-app:task-app@cluster0.e7dk1su.mongodb.net/?retryWrites=true&w=majority')
-
-// const User = mongoose.model('User',{
-//     name: {
-//         type:String,
-//         required:true,
-//         trim:true
-
-//     },
-//     password:{
-//         type: String,
-//         reqired:true,
-//         trim:true,
-//         minlength:7,
-//         validate(value){
-//             if (value.toLowerCase().includes('password')){
-//                 throw new Error('Password cannot contain "password" ')
-//             }
-//         }
-
-
-//     },
-//     email: {
-//         type:String,
-//         required:true,
-//         trim:true,
-//         lowercase:true,
-//         validate(value){
-//             if (!validator.isEmail(value)) {
-//                 throw new Error('Email is Invalid!')
-//             }
-//         }
-//     },
-//     age:{
-//         type:Number,
-//         default:0,
-//         validate(value){
-//             if(value<0){
-//                 throw new Error('Age must be a postive number')
-//             }
-//         }
-//     }
-// })
-
-// const me =new User({
-//     name: '   Nadir  ', 
-//     password:'Pass132',
-//     email:'    Nadir@gmail.com  '
-    
-// })
-
-// me.save().then(() =>{
-//     console.log(me)
-// }).catch((error)=>{
-//     console.log('Error!',error)
-// })
-
-
 
 
  const Task =mongoose.model('Task',{
@@ -73,15 +17,5 @@ mongoose.connect('mongodb+srv://task-app:task-app@cluster0.e7dk1su.mongodb.net/?
     }
 } )
 
-const task =new Task({
-    description:'     Learn man library'
-    
-})
-
-task.save().then(()=>{
-    console.log(task)
-}).catch((error)=>{
-    console.log(error)
-})
 
 
