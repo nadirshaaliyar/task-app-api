ef43f541b53e19101b4b12ea2981782c6a877d31
Nadirsha Aliyar <42641677+nadirshaaliyar@users.noreply.github.com>
2022-11-02 13:49:30 +0530

    rest api for task creation🚀

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,18 +4,6 @@ const mongoose =require('mongoose')
 mongoose.connect('mongodb+srv://task-app:task-app@cluster0.e7dk1su.mongodb.net/?retryWrites=true&w=majority')
 
 
- const Task =mongoose.model('Task',{
-    description:{
-        type:String,
-        required:true,
-        trim:true
-
-    },
-    completed:{
-        type:Boolean,
-        default:false
-    }
-} )
 
 
 
